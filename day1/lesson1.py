a42bcec6b07211636c0c61001a195515ec64912e
didgori92 <160664257+didgori92@users.noreply.github.com>
2024-02-22 23:10:02 +0400

    Add files via upload

---
+++ day1/lesson1.py
@@ -0,0 +1,12 @@
+name = "levani"
+surname = "gendzekhadze"
+age = 16
+height = 171
+
+
+
+print("hello, my name is" + " " + name + " " + surname + ", " + "i am" + " " + str(age) + " " + "years old, my height is" + " " + str(height) +" " + "centimeters, and i have high hopes here at GOA to learn and create a good future for myself and my family")
+
+
+
+
