16c190d34aaa12c946c45dd5d21b229f9da20e00
didgori92 <160664257+didgori92@users.noreply.github.com>
2024-02-25 13:40:41 +0400

    Add files via upload

---
+++ homework01.py
@@ -0,0 +1,11 @@
+name = input("what is your name: ")
+surname = input("what is your surname: ")
+todays_date = 2024
+date_of_birth = input("what year were you born in: ")
+age = todays_date - int(date_of_birth)
+print("users full name is" + " " + name + " " + surname + " " + "and is" + " " + str(age) + " " + "years old")
+
+
+
+
+
