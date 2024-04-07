2da83d316ea07806ec40ae507893afeefba30ada
didgori92 <160664257+didgori92@users.noreply.github.com>
2024-04-07 20:19:03 +0400

    Add files via upload

---
+++ day 10/hw1.py
@@ -0,0 +1,10 @@
+academy = input("which programming academy did you study in? ")
+
+if academy == "GOA":
+    print("you made the right choice")
+else:
+    print("you made the wrong choice")
+
+
+
+
